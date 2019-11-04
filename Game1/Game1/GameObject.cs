76844cca3d7a311a435269bf383f374e93eb2bf4
Coala2407 <36776480+Coala2407@users.noreply.github.com>
2019-11-04 13:50:19 +0100

    +gameObject

---
+++ Game1/Game1/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    public abstract class GameObject
+    {
+        //Fields
+
+        //Constructor
+
+        //Abstract
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void LoadContent(ContentManager content);
+
+        //Virtual
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
