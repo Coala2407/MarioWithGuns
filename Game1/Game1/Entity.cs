04fd3e095d7ba5f78a63993b375678519343dfbc
Coala2407 <36776480+Coala2407@users.noreply.github.com>
2019-11-05 13:26:39 +0100

    +Entity Class. *GameObject

--- Game1/Game1/Entity.cs
+++ Game1/Game1/Entity.cs
@@ -8,16 +8,64 @@ using Microsoft.Xna.Framework.Content;
 
 namespace Game1
 {
-    class Entity : GameObject
+    abstract class Entity : GameObject
     {
+        //Fields
+        private Vector2 velocity;
+        private float moveSpeed;
+        private float shootCooldown;
+        private int health;
+
+        //From GameObject:
         public override void LoadContent(ContentManager content)
-        {
-            throw new NotImplementedException();
-        }
+        {}
 
         public override void Update(GameTime gameTime)
+        {}
+         
+        /// <summary>
+        /// Shoot a bullet
+        /// </summary>
+        public abstract void Shoot();
+
+        /// <summary>
+        /// Runs on collision with another entity
+        /// </summary>
+        public abstract void OnCollision();
+
+        /// <summary>
+        /// Removes entity
+        /// </summary>
+        public abstract void Die();
+
+        /// <summary>
+        /// Move entity
+        /// </summary>
+        public virtual void Move()
+        {}
+
+        /// <summary>
+        /// Get/make a collision box for the entity
+        /// </summary>
+        public virtual Rectangle GetCollisionBox
         {
-            throw new NotImplementedException();
+            get
+            {
+                return new Rectangle();
+            }
+        }
+
+        //Checks for collision with another entity
+        public void CheckCollision(Entity otherEntity)
+        {}
+
+        /// <summary>
+        /// Updates the health of the entity
+        /// </summary>
+        /// <returns>The the new health value</returns>
+        protected int UpdateHealth()
+        {
+            return health;
         }
     }
 }
