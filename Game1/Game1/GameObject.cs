13bce4cb5419c26cc9fd3fff8b3c4b6ea709f038
Coala2407 <36776480+Coala2407@users.noreply.github.com>
2019-11-05 12:58:09 +0100

    added classes

--- Game1/Game1/GameObject.cs
+++ Game1/Game1/GameObject.cs
@@ -2,14 +2,10 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Game1
 {
-    public abstract class GameObject
+    abstract class GameObject
     {
         //Fields
         protected Texture2D sprite;
