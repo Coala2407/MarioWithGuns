c35cc2f08f25978dee058f17c6caf7410a0c39eb
Coala2407 <36776480+Coala2407@users.noreply.github.com>
2019-11-12 13:46:40 +0100

    Revert "Revert "enemy collision and death!""
    This reverts commit b4bc811437ba91f486c76ee151989d4dfb388af3.

--- Game1/Game1/Player.cs
+++ Game1/Game1/Player.cs
@@ -58,7 +58,7 @@ namespace Game1
             if (isJumping)
             {
                 //Starts jump timer to allow jumps
-                if ((!wasJumping && isOnGround) || jumpTime > 0.0f)
+                if ((/*!wasJumping &&*/ isOnGround) || jumpTime > 0.0f)
                 {
                     jumpTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
                 }
@@ -191,10 +191,9 @@ namespace Game1
                 else
                 {
                     //Player is not properly on the platform
+                    isOnGround = false;
                     //Cancel potential jump (hitting your head on a platform)
                     jumpTime = 0.0f;
-                    //Not on the ground
-                    isOnGround = false;
                 }
             }
         }
@@ -225,7 +224,7 @@ namespace Game1
             {
                 OnCollision(otherEntity);
             }
-            else if(currentPlatform != null && otherEntity == currentPlatform)
+            else if (currentPlatform != null && otherEntity == currentPlatform)
             {
                 isOnGround = false;
                 currentPlatform = null;
