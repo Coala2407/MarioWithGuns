c6a9801caf5de9af137fe6c1f915a54715201b03
Mick <mickmortensen@hotmail.com>
2019-11-12 13:26:19 +0100

    sbf<ihu

--- Game1/Game1/GameWorld.cs
+++ Game1/Game1/GameWorld.cs
@@ -110,8 +110,9 @@ namespace Game1
             EntityList.Add(new Platform(new Vector2(0, 700), (int)screenSize.X - 1, 50));
             EntityList.Add(new Platform(new Vector2(0, 600), 500, 50));
             GameObjectList.Add(new Crosshair());
-            GameObjectList.Add(new BackGround("download"));
-            GameObjectList.Add(new BackGround("119", 300));
+            GameObjectList.Add(new BackGround("Backgroundlayer03",0));
+            //GameObjectList.Add(new BackGround("Backgroundlayer02", 50));
+            //GameObjectList.Add(new BackGround("Backgroundlayer01", 55));
 
             base.Initialize();
         }
