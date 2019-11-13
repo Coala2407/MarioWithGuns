857c8824b3540219a58bcb4d0c5799e6363bb8f0
Mick <mickmortensen@hotmail.com>
2019-11-13 10:19:16 +0100

    Update GameWorld.cs

--- Game1/Game1/GameWorld.cs
+++ Game1/Game1/GameWorld.cs
@@ -88,6 +88,7 @@ namespace Game1
         public GameWorld()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
             Content.RootDirectory = "Content";
         }
 
@@ -122,8 +123,8 @@ namespace Game1
             EntityList.Add(new Enemy(new Vector2(200, 720)));
             GameObjectList.Add(new Crosshair());
             GameObjectList.Add(new BackGround("Backgroundlayer03",0));
-            //GameObjectList.Add(new BackGround("Backgroundlayer02", 50));
-            //GameObjectList.Add(new BackGround("Backgroundlayer01", 55));
+            GameObjectList.Add(new BackGround("Backgroundlayer02", -50));
+            GameObjectList.Add(new BackGround("Backgroundlayer01", -55));
 
             base.Initialize();
         }
